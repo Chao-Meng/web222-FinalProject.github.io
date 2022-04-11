809eb6f420d303bfb1224629c97e2a9012c529db
CHAO MENG <90300602+Chao-Meng@users.noreply.github.com>
2022-04-11 18:01:23 -0400

    Add files via upload

---
+++ FinalAss.js
@@ -0,0 +1,12 @@
+function inputBox() {
+    var str = prompt("Please enter the hourly rate", "");
+
+    if (str != null && str != "") {
+    alert("Thank you, I will contact with you soon.")
+    }
+  }
+function inputText(){
+    if(value=="Question"||"Comments"){
+        
+    }
+}
